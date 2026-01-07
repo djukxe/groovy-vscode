aae29ff80d39f19ec1fc6a156fe667f213b479b6
djukxe <julien.tahon0@gmail.com>
2026-01-07 13:35:47 +0900

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/test'],
+  roots: ['<rootDir>/server/src', '<rootDir>/server/test'],
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
